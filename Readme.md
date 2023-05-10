3d46ac0ea013af357aed1b190b6f876cec6209a2
Elizaveta Nemzer <104012082+elizernem@users.noreply.github.com>
2023-05-10 14:15:36 +0300

    исправляет сборку проекта

--- Readme.md
+++ Readme.md
@@ -2,6 +2,6 @@
 
 **Папка с исходными файлами — `source/`.**
 
-Запуск проекта: npm run build
+Запуск проекта: npm run start
 
 [Макет и ТЗ](https://www.figma.com/file/LP1CVONTsu2altE9cjngaf/%D0%A2%D0%B5%D1%81%D1%82%D0%BE%D0%B2%D0%BE%D0%B5?node-id=1%3A3)
--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,7 @@ import postcss from 'gulp-postcss';
 import csso from "postcss-csso";
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
-import htmlmin from "gulp-htmlmin";
-import svgo from "gulp-svgmin";
-import svgstore from "gulp-svgstore";
 import { deleteAsync } from "del";
-import { htmlValidator } from "gulp-w3c-html-validator";
 import rename from "gulp-rename";
 
 // Styles
@@ -23,49 +19,8 @@ export const styles = () => {
     .pipe(sass())
     .pipe(postcss([autoprefixer(), csso()]))
     .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("build/css", { sourcemaps: "." }))
+    // .pipe(gulp.dest("build/css", { sourcemaps: "." }))
     .pipe(browser.stream());
-};
-
-// HTML
-
-const html = () => {
-  return gulp
-    .src("source/*.html")
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest("build"));
-};
-
-export const validator = () => {
-  return gulp
-    .src("build/*.html")
-    .pipe(htmlValidator.analyzer())
-    .pipe(htmlValidator.reporter());
-}
-
-const svg = () =>
-  gulp
-    .src("source/img/*.svg")
-    .pipe(svgo())
-    .pipe(gulp.dest("build/img"));
-
-const sprite = () => {
-  return gulp
-    .src("source/img/sprite/*.svg")
-    .pipe(svgo())
-    .pipe(
-      svgstore({
-        inlineSvg: true,
-      })
-    )
-    .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("build/img"));
-};
-
-// Clean
-
-const reset = () => {
-  return deleteAsync("build");
 };
 
 // Server
@@ -73,7 +28,7 @@ const reset = () => {
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: "build",
+      baseDir: "source",
     },
     cors: true,
     notify: false,
@@ -93,24 +48,13 @@ const reload = (done) => {
 
 const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
-  gulp.watch("source/*.html", gulp.series(html, reload));
-  gulp.watch("source/img/sprite/*.svg", gulp.series(sprite, reload));
-  gulp.watch("source/img/*.svg", gulp.series(svg, reload));
+  gulp.watch("source/*.html", gulp.series(reload));
 }
 
-
-// Build
-
-export const build = gulp.series(
-  reset,
-  gulp.parallel(styles, html, svg, sprite),
-  gulp.series(server, watcher)
-);
 
 // Default
 
 export default gulp.series(
-  reset,
-  gulp.parallel(styles, html, svg, sprite),
+  gulp.parallel(styles),
   gulp.series(server, watcher)
 );
